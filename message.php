8a9706a5d4809838124657bf986db70ccb00f4df
JNBetteraij <95929522+JNBetteraij@users.noreply.github.com>
2023-11-06 12:05:44 +0100

    Added guidv4.php: contains a function to generate unique ID's used for message identification. Changed message.php: message object will now generate a GUID in the constructor unless one is provided

--- message.php
+++ message.php
@@ -1,12 +1,31 @@
 <?php
+include_once 'guidv4.php';
 class Message implements JsonSerializable{
     public $name;
     public $message;
+    public $id;
 
-    function __construct(string $name,$message)
+    function __construct(string $name, string $message, string $ID = null)
     {
         $this->name = $name;
         $this->message = $message;
+        $this->id = $ID ?? $this->generateID();
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function getMessage(){
+        return $this->message;
+    }
+
+    public function getID(){
+        return $this->id;
+    }
+
+    function generateID(){
+        return guidv4();
     }
 
     function jsonSerialize(): mixed
